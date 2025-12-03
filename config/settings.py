e94a4a5a4e1ae64606a0c05e04f4a9f8d3ca7d67
David Cabezas <david.cabezas03@inacapmail.cl>
2025-12-03 17:51:38 -0300

    Resposivo

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Configuración de Archivos Subidos (Media)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
